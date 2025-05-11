04952ee527be5f33b3b013bd1c5f2dc5e057382b
geooooorge12 <whwlgns5767@naver.com>
2025-05-11 18:15:00 +0900

    Update server.py

--- server.py
+++ server.py
@@ -9,12 +9,9 @@ def home():
 
 @app.route('/files')
 def list_files():
-    directory = 'json_files'  # 실제 JSON 파일이 있는 디렉터리 이름에 맞게 수정
+    directory = 'json_files'  # 실제 json 파일 폴더 경로
     try:
         files = [f for f in os.listdir(directory) if f.endswith('.json')]
         return jsonify(files)
     except FileNotFoundError:
         return jsonify({"error": "Directory not found"}), 404
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
